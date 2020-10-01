d1bb3e96837e5a55cd9d916770353cff7a271d13
Tulsonic <tulgar.dinc@gmail.com>
2020-10-01 19:46:10 +0300

    Setup

---
+++ AI/Assets/scripts/Car.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Vector3 inVel = Vector3.zero;
+    public Vector3 inAcel = Vector3.zero;
+
+    Vector3 vel = Vector3.zero;
+    Vector3 acel = Vector3.zero;
+
+    Rigidbody rb; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        vel = inVel;
+        acel = inAcel;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vel += acel;
+        rb.position += vel;
+        Debug.Log(rb.position);
+    }
+}
